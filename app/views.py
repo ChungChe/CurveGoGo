46f8786f54377c64a90dcfa9734805927a302a15
ChungChe <r94943159@ntu.edu.tw>
2016-04-19 03:42:54 +0000

    new file:   db_util.py 	modified:   templates/index.html 	modified:   views.py

--- app/views.py
+++ app/views.py
@@ -1,12 +1,42 @@
 from flask import Flask
 from flask import render_template
+import sqlite3 as db
+import db_util
+
 app = Flask(__name__)
+
+def query(start, end, machine_list):
+	try:
+		con = db.connect('curve.db')
+		print('Connect to curve.db')
+		cur = con.cursor()
+		outputList = db_util.get_data(cur, start, end, machine_list)
+	except db.Error, e:
+		if con:
+			con.rollback()
+		print("Error %s" % e.args[0])
+		sys.exit(1)
+	finally:
+		if cur:
+			cur.close()
+		if con:
+			con.close()
+		return outputList
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
+	start = '2016-04-18 12:42:53'
+	end = '2016-04-18 16:43:07'
+	machine_list = [2, 9]
+	outputList = query(start, end, machine_list)
+	for element in outputList:
+		print(element)
+	
 	user = {'nickname': 'Shit'}
-	return render_template('index.html', title = path, user=user)
+	return render_template('index.html', 
+			title = path, 
+			user = user, output = outputList )
 	
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
