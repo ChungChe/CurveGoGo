734d773e077da900d2e7addebfa2ca6051982f45
ChungChe <r94943159@ntu.edu.tw>
2016-04-19 03:01:17 +0000

    modified:   db.py 	DB Reader

--- app/db.py
+++ app/db.py
@@ -28,11 +28,16 @@ def insert(cur, m_id, value):
 	data = [m_id, value]
 	cur.execute("insert into a_curve values (?,?,datetime('now'))", data)
 
-def random_insert():
-	try:
-		con = db.connect('curve.db')
-		print('Connect to curve.db')
-		cur = con.cursor()
+def get_data(cur, start, end, machinelist):
+	st = "(" + ",".join(["?"] * len(machinelist)) + ")"
+	print("machine: " + str(machinelist) + " between " + start + " and " + end)
+	input_list = [start, end] + machinelist
+	cur.execute("select * from a_curve where timestamp between ? and ? and machine_id in " + st, input_list)
+	outputList = cur.fetchall()
+	for element in outputList:
+		print(element)
+
+def random_insert(con, cur):
 		for i in range(200): 
 			wait_s = random.randint(1, 4)
 			time.sleep(wait_s);
@@ -41,6 +46,14 @@ def random_insert():
 			print('id: ' + str(m_id) + ', value: ' + str(v))
 			insert(cur, m_id, v)
 		con.commit()
+
+def main():
+	try:
+		con = db.connect('curve.db')
+		print('Connect to curve.db')
+		cur = con.cursor()
+		machine_list = [22, 2, 5, 8]
+		get_data(cur, '2016-04-18 12:42:53', '2016-04-18 16:43:07', machine_list)
 	except db.Error, e:
 		if con:
 			con.rollback()
@@ -51,7 +64,6 @@ def random_insert():
 			cur.close()
 		if con:
 			con.close()
-
 	
 #create_db(dbName)
-random_insert()
+main()
